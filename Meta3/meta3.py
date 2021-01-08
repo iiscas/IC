5dc1f2dbf87b4f4baa34ed8cf4fe117dc7549a8c
Isabel Castro <isabelcastro4@outlook.pt>
2021-01-08 02:10:45 +0000

    Mostra cada classe em gráfico
    Não dá para fazer reshape

--- Meta3/meta3.py
+++ Meta3/meta3.py
@@ -7,21 +7,41 @@ Created on Thu Jan  7 01:56:45 2021
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
-from tensorflow.python import keras
+from tensorflow import keras
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, Flatten, Conv2D, Dropout, MaxPooling2D
 from IPython.display import SVG
 from keras.utils.vis_utils import model_to_dot
 from keras.utils import plot_model
 import seaborn as sns
+import matplotlib.pyplot as plt
+from PIL import Image
 
+from sklearn.model_selection import train_test_split
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.utils import to_categorical
+
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
+from keras.optimizers import Adam
+
+from keras.callbacks import LearningRateScheduler
+from keras.callbacks import EarlyStopping
 
 
 
 import SwarmPackagePy
 
-
-
+IMG_ROWS = 28
+IMG_COLS = 28
+NUM_CLASSES = 10
+TEST_SIZE = 0.2
+RANDOM_STATE = 2018
+#Model
+NO_EPOCHS = 50
+BATCH_SIZE = 128
 train= pd.read_csv("fashion-mnist_train.csv")
 test= pd.read_csv("fashion-mnist_test.csv")
 
@@ -31,8 +51,6 @@ print("Fashion MNIST test -  rows:",test.shape[0]," columns:", test.shape[1])
 
 train.head(5)
 
-print("train missing values:", train.isnull().any().sum())
-print("test missing values:", test.isnull().any().sum())
 
 
 labels = {0 : "T-shirt/top", 1: "Trouser", 2: "Pullover", 3: "Dress", 4: "Coat",
@@ -54,3 +72,58 @@ def get_classes_distribution(data):
         print("{:<20s}:   {} or {}%".format(label, count, percent))
         
 get_classes_distribution(train)
+
+X = train.iloc[:,1:] #taking all but the first row
+Y = train.iloc[:,0] #taking only the first row as this is the label
+
+class_names = ['T_shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+#imagens
+plt.figure(figsize=(10, 10))
+for i in range(36):
+    plt.subplot(6, 6, i + 1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(X.loc[i].values.reshape((28,28))) #calling the .values of each row
+    label_index = int(Y[i]) #setting as an int as the number is stored as a string
+    plt.title(class_names[label_index])
+plt.show()
+
+#grafico de barras por cada classe
+def plot_label_per_class(data):
+
+    f, ax = plt.subplots(1,1, figsize=(12,4))
+    g = sns.countplot(data.label, order = data["label"].value_counts().index)
+    g.set_title("Number of labels for each class")
+
+    for p, label in zip(g.patches, data["label"].value_counts().index):
+        g.annotate(labels[label], (p.get_x(), p.get_height()+0.1))
+    plt.show()  
+    
+plot_label_per_class(train)
+get_classes_distribution(test)
+plot_label_per_class(test)
+
+# data preprocessing
+def data_preprocessing(raw):
+    out_y = keras.utils.to_categorical(raw.label, NUM_CLASSES)
+    num_images = raw.shape[0]
+    x_as_array = raw.values[:,1:]
+    x_shaped_array = x_as_array.reshape(num_images, IMG_ROWS, IMG_COLS, 1)
+    out_x = x_shaped_array / 255
+    return out_x, out_y
+
+
+X, y = data_preprocessing(train)
+X_test, y_test = data_preprocessing(test)
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+
+
+print("Fashion MNIST train -  rows:",X_train.shape[0]," columns:", X_train.shape[1:4])
+print("Fashion MNIST valid -  rows:",X_val.shape[0]," columns:", X_val.shape[1:4])
+print("Fashion MNIST test -  rows:",X_test.shape[0]," columns:", X_test.shape[1:4])
+#reshape(examples, height, width, channels)
+x_train = train.values.reshape(-1, 28, 28, 3)
+x_test = test.values.reshape(-1, 28, 28, 3)
