23d865d8759668839f90e5758c8ce85d64e81f6a
zezzz555 <75699651+zezzz555@users.noreply.github.com>
2021-01-12 23:24:40 +0000

    CNN

--- Meta3/meta3.py
+++ Meta3/meta3.py
@@ -1,60 +1,117 @@
 import numpy as np
 import pandas as pd
-import os
-
-import matplotlib.pyplot as plt
-
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, Dropout, MaxPooling2D
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+import seaborn as sns
+import matplotlib.pyplot as plt
+%matplotlib inline 
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+from plotly import tools
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+init_notebook_mode(connected=True)
 
-from keras.preprocessing.image import ImageDataGenerator
-from keras.utils import to_categorical
 
-from keras.models import Sequential, load_model
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D, BatchNormalization
-from keras.optimizers import Adam
-
-from keras.callbacks import LearningRateScheduler
-from keras.callbacks import EarlyStopping
-import SwarmPackagePy
-from SwarmPackagePy import testFunctions as tf
-from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import confusion_matrix
-
-INPUT_SHAPE = 784
-NUM_CATEGORIES = 10
-
-LABEL_DICT = {
- 0: "T-shirt/top",
- 1: "Trouser",
- 2: "Pullover",
- 3: "Dress",
- 4: "Coat",
- 5: "Sandal",
- 6: "Shirt",
- 7: "Sneaker",
- 8: "Bag",
- 9: "Ankle boot"
-}
+IMG_ROWS = 28
+IMG_COLS = 28
+NUM_CLASSES = 10
+TEST_SIZE = 0.2
+RANDOM_STATE = 2018
+#Model
+NO_EPOCHS = 50
+BATCH_SIZE = 128
 
 # LOAD THE RAW DATA
-train = pd.read_csv('fashion-mnist_train.csv')
-test = pd.read_csv('fashion-mnist_test.csv')
+train_data = pd.read_csv('fashion-mnist_train.csv')
+test_data = pd.read_csv('fashion-mnist_test.csv')
 
-print(train.shape)
-print(test.shape)
+print("Fashion MNIST train -  rows:",train_data.shape[0]," columns:", train_data.shape[1])
+print("Fashion MNIST test -  rows:",test_data.shape[0]," columns:", test_data.shape[1])
+
+# data preprocessing
+def data_preprocessing(raw):
+    out_y = keras.utils.to_categorical(raw.label, NUM_CLASSES)
+    num_images = raw.shape[0]
+    x_as_array = raw.values[:,1:]
+    x_shaped_array = x_as_array.reshape(num_images, IMG_ROWS, IMG_COLS, 1)
+    out_x = x_shaped_array / 255
+    return out_x, out_y
+
+# prepare the data
+X, y = data_preprocessing(train_data)
+X_test, y_test = data_preprocessing(test_data)
+
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=TEST_SIZE, random_state=RANDOM_STATE)
+
+print("Fashion MNIST train -  rows:",X_train.shape[0]," columns:", X_train.shape[1:4])
+print("Fashion MNIST valid -  rows:",X_val.shape[0]," columns:", X_val.shape[1:4])
+print("Fashion MNIST test -  rows:",X_test.shape[0]," columns:", X_test.shape[1:4])
+
+#Model
+model = Sequential()
+# Add convolution 2D
+model.add(Conv2D(32, kernel_size=(3, 3),
+                 activation='relu',
+                 kernel_initializer='he_normal',
+                 input_shape=(IMG_ROWS, IMG_COLS, 1)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(64, 
+                 kernel_size=(3, 3), 
+                 activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(128, (3, 3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(NUM_CLASSES, activation='softmax'))
 
 
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer='adam',
+              metrics=['accuracy'])
 
-X = train.iloc[:,1:] 
-Y = train.iloc[:,0] 
+#model.summary()
+
+train_model = model.fit(X_train, y_train,
+                  batch_size=BATCH_SIZE,
+                  epochs=NO_EPOCHS,
+                  verbose=1,
+                  validation_data=(X_val, y_val))
+
+#test prediction accuracy
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+#visualize correct images
+def plot_images(data_index,cmap="Blues"):
+    # Plot the sample images now
+    f, ax = plt.subplots(4,4, figsize=(15,15))
+
+    for i, indx in enumerate(data_index[:16]):
+        ax[i//4, i%4].imshow(X_test[indx].reshape(IMG_ROWS,IMG_COLS), cmap=cmap)
+        ax[i//4, i%4].axis('off')
+        ax[i//4, i%4].set_title("True:{}  Pred:{}".format(labels[y_true[indx]],labels[predicted_classes[indx]]))
+    plt.show()    
+    
+plot_images(correct, "Greens")
+
+#incorrect
+plot_images(incorrect, "Reds")
 
 
-x_train , x_test , y_train , y_test = train_test_split(X, Y , test_size=0.1)
-
+"""
 class_names = ['T_shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 
 
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+
 
 plt.figure(figsize=(10, 10))
 for i in range(36):
@@ -68,14 +125,14 @@ for i in range(36):
 plt.show()
 
 
-x_train = x_train.values.reshape(-1, 28, 28, 1)
-x_test = x_test.values.reshape(-1, 28, 28, 1)
+x_train = x_train.values.reshape(-1,28,28,1)
+x_test = x_test.values.reshape(-1,28,28,1)
 
 
 x_train = x_train.astype("float32")/255
 x_test = x_test.astype("float32")/255
 
-
+    
 y_train = to_categorical(y_train, num_classes=10)
 y_test = to_categorical(y_test, num_classes=10)
 
@@ -125,3 +182,4 @@ alg = SwarmPackagePy.gsa(n=10, lb = lbb, ub = ubb, function=f,  dimension=dimens
 
 um, dois = alg.get_Gbest()
 print ("BEST RESULTS: ",alg.get_Gbest())
+"""
